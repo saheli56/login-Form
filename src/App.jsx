aab026b705945bb4e5567fd930f6cc370900ff18
Saheli <sahelidas0506@gamil.com>
2025-04-14 11:57:18 +0530

    added eye button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
 const App = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className="login-container">
       <h2 className="form-title">log in with</h2>
@@ -15,18 +21,18 @@ const App = () => {
       <p className="separator"><span>or</span></p>
 
       <form action="#" className="login-form">
-        <div className="input-wapper">
+        <div className="input-wrapper">
           <input type="email" placeholder="Email address" className="input-field" required />
           <i className="material-symbols-rounded">mail</i>
         </div>
       
     
-    <div className="input-wapper">
+    <div className="input-wrapper">
           <input type="password" placeholder="Password" className="input-field" required />
-          <i className="material-symbols-rounded">lock</i>
+          <i className="material-symbols-rounded eye">lock</i>
         </div>
            <a href="#" className="forgot-pass-link">Forgot Password?</a>
-           <button className="login-buttton">Log In</button>
+           <button className="login-button">Log In</button>
       </form>
 
       <p className="signup-text">Don't have an account? <a href="#">Signup now</a></p>
---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,7 @@
+const SocialLogin = () => {
+  return (
+    
+  )
+}
+
+export default SocialLogin
